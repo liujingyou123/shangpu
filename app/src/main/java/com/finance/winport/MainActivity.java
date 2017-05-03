eb11a752c13691cf6a3fc27dde028463d1ded45f
葛进 <gejin@oriental-finance.com>
2017-05-03 10:20:54 +0800

    界面主框架搭建

--- app/src/main/java/com/finance/winport/MainActivity.java
+++ app/src/main/java/com/finance/winport/MainActivity.java
@@ -1,13 +1,98 @@
 package com.finance.winport;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.finance.winport.base.BaseActivity;
+import com.finance.winport.tab.BusinessFragment;
+import com.finance.winport.tab.HomeFragment;
+import com.finance.winport.tab.MineFragment;
+import com.finance.winport.tab.ServiceFragment;
+import com.finance.winport.view.BottomTabView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity implements BottomTabView.OnTabSelectedListener {
+
+    static final int HOME = 0;
+    static final int SERVICE = 1;
+    static final int BUSINESS = 2;
+    static final int MINE = 3;
+    private FragmentManager fm;
+    @BindView(R.id.tabView)
+    BottomTabView tabView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        fm = getSupportFragmentManager();
+        tabView.setOnTabSelectedListener(this);
+        tabView.setTabResIds(new int[]{R.drawable.selector_bottom_tab_home
+                , R.drawable.selector_bottom_tab_service, R.drawable
+                .selector_bottom_tab_business, R.drawable
+                .selector_bottom_tab_mine});
+        onTabSelected(HOME);
+    }
+
+    @Override
+    public void onTabSelected(int index) {
+
+        switch (index) {
+            case HOME:
+                handleHome(index);
+                break;
+            case SERVICE:
+                handleHouse(index);
+                break;
+            case BUSINESS:
+                handleEntrust(index);
+                break;
+            case MINE:
+                handleMine(index);
+                break;
+        }
+    }
+
+    private void handleHome(int index) {
+        HomeFragment homeFragment = (HomeFragment) fm
+                .findFragmentByTag(HomeFragment.class.getName());
+        if (homeFragment == null) {
+            homeFragment = new HomeFragment();
+        }
+        addFragment(homeFragment, false);
+    }
+
+    private void handleEntrust(int index) {
+        ServiceFragment serviceFragment = (ServiceFragment) fm
+                .findFragmentByTag(ServiceFragment.class.getName());
+        if (serviceFragment == null) {
+            serviceFragment = new ServiceFragment();
+        }
+        addFragment(serviceFragment, false);
+    }
+
+    private void handleHouse(int index) {
+        BusinessFragment businessFragment = (BusinessFragment) fm
+                .findFragmentByTag(BusinessFragment.class.getName());
+        if (businessFragment == null) {
+            businessFragment = new BusinessFragment();
+        }
+        addFragment(businessFragment, false);
+    }
+
+    private void handleMine(int index) {
+        MineFragment mineFragment = (MineFragment) fm
+                .findFragmentByTag(MineFragment.class.getName());
+        if (mineFragment == null) {
+            mineFragment = new MineFragment();
+        }
+        addFragment(mineFragment, false);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/finance/winport/tab/BusinessFragment.java
@@ -0,0 +1,59 @@
+package com.finance.winport.tab;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.finance.winport.R;
+import com.finance.winport.base.BaseFragment;
+import com.finance.winport.util.StringUtil;
+import com.finance.winport.util.TextViewUtil;
+import com.finance.winport.util.UnitUtil;
+import com.finance.winport.view.CountDownButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ *
+ *
+ */
+
+public class BusinessFragment extends BaseFragment {
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.business_fragment, container, false);
+        return root;
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
