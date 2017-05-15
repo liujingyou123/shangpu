a883762e8ecb24ae9493580482b1edc990b0da96
刘景友 <liujy@oriental-finance.com>
2017-05-15 10:50:33 +0800

    网络基础

--- app/src/main/java/com/finance/winport/net/NetUtils.java
+++ app/src/main/java/com/finance/winport/net/NetUtils.java
@@ -1,8 +1,10 @@
 package com.finance.winport.net;
 
+import com.finance.winport.BuildConfig;
+
 public class NetUtils {
 
-    private static String baseUrl = "";
+    private static String baseUrl = BuildConfig.BASEURL;
 
     public static String baseUrl() {
         return baseUrl;
