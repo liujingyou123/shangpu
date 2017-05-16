7c08c193b0492ec347ee342498eb4d35761437d0
刘景友 <liujy@oriental-finance.com>
2017-05-16 15:32:13 +0800

    友盟分享

--- app/src/main/java/com/finance/winport/dialog/ShareDialog.java
+++ app/src/main/java/com/finance/winport/dialog/ShareDialog.java
@@ -1,5 +1,6 @@
 package com.finance.winport.dialog;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -11,14 +12,13 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.finance.winport.R;
+import com.finance.winport.log.XLog;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import cn.sharesdk.onekeyshare.OnekeyShare;
-import cn.sharesdk.sina.weibo.SinaWeibo;
-import cn.sharesdk.tencent.qq.QQ;
-import cn.sharesdk.wechat.friends.Wechat;
-import cn.sharesdk.wechat.moments.WechatMoments;
 
 /**
  * Created by liuworkmac on 17/5/8.
@@ -51,52 +51,50 @@ public class ShareDialog extends Dialog {
         window.setWindowAnimations(R.style.dialogWheel);
     }
 
-    private void showShare(String platform) {
-        final OnekeyShare oks = new OnekeyShare();
-        //指定分享的平台，如果为空，还是会调用九宫格的平台列表界面
-        if (platform != null) {
-            oks.setPlatform(platform);
-        }
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle("标题");
-        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
-        // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
-        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
-        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite("ShareSDK");
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
-
-        //启动分享
-        oks.show(mContext);
+    private void showShare(SHARE_MEDIA platform) {
+        new ShareAction((Activity) mContext).setPlatform(platform)
+                .withText("hello")
+                .setCallback(umShareListener)
+                .share();
     }
 
     @OnClick({R.id.tv_weixin, R.id.tv_pengyou, R.id.tv_weibo, R.id.tv_qq})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_weixin:
-                showShare(Wechat.NAME);
+                showShare(SHARE_MEDIA.WEIXIN);
                 break;
             case R.id.tv_pengyou:
-                showShare(WechatMoments.NAME);
+                showShare(SHARE_MEDIA.WEIXIN_CIRCLE);
                 break;
             case R.id.tv_weibo:
-                showShare(SinaWeibo.NAME);
+                showShare(SHARE_MEDIA.SINA);
                 break;
             case R.id.tv_qq:
-                showShare(QQ.NAME);
+                showShare(SHARE_MEDIA.QQ);
                 break;
         }
     }
+
+    private UMShareListener umShareListener = new UMShareListener() {
+        @Override
+        public void onStart(SHARE_MEDIA share_media) {
+            XLog.e("onStart");
+        }
+
+        @Override
+        public void onResult(SHARE_MEDIA share_media) {
+            XLog.e("onResult");
+        }
+
+        @Override
+        public void onError(SHARE_MEDIA share_media, Throwable throwable) {
+            XLog.e("onError");
+        }
+
+        @Override
+        public void onCancel(SHARE_MEDIA share_media) {
+            XLog.e("onCancel");
+        }
+    };
 }
--- app/src/main/java/com/finance/winport/home/ShopDetailActivity.java
+++ app/src/main/java/com/finance/winport/home/ShopDetailActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.finance.winport.R;
 import com.finance.winport.base.BaseActivity;
@@ -19,7 +20,15 @@ import com.finance.winport.util.UnitUtil;
 import com.finance.winport.view.PositionScrollView;
 import com.finance.winport.view.ScrollTabView;
 import com.finance.winport.view.imagepreview.ImagePreviewActivity;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.media.UMWeb;
+import com.umeng.socialize.shareboard.SnsPlatform;
+import com.umeng.socialize.utils.ShareBoardlistener;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,6 +82,8 @@ public class ShopDetailActivity extends BaseActivity {
     private ShareDialog shareDialog;
     private NoticeDialog mNoticeDialog;
 
+    private UMShareListener mShareListener;
+    private ShareAction mShareAction;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -80,6 +91,7 @@ public class ShopDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_shopdetail);
         ButterKnife.bind(this);
         init();
+        shareInit();
     }
 
     private void init() {
@@ -196,10 +208,11 @@ public class ShopDetailActivity extends BaseActivity {
                 finish();
                 break;
             case R.id.tv_share:
-                if (shareDialog == null) {
-                    shareDialog = new ShareDialog(this);
-                }
-                shareDialog.show();
+//                if (shareDialog == null) {
+//                    shareDialog = new ShareDialog(this);
+//                }
+//                shareDialog.show();
+                mShareAction.open();
                 break;
             case R.id.tv_shop_more:
                 Intent intent = new Intent(ShopDetailActivity.this, ShopMoreActivity.class);
@@ -239,6 +252,111 @@ public class ShopDetailActivity extends BaseActivity {
                 }
                 break;
 
+        }
+    }
+
+    private void shareInit() {
+        mShareListener = new CustomShareListener(this);
+        /*增加自定义按钮的分享面板*/
+        mShareAction = new ShareAction(ShopDetailActivity.this).setDisplayList(
+                SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE, SHARE_MEDIA.WEIXIN_FAVORITE,
+                SHARE_MEDIA.SINA, SHARE_MEDIA.QQ, SHARE_MEDIA.QZONE,
+                SHARE_MEDIA.ALIPAY, SHARE_MEDIA.RENREN, SHARE_MEDIA.DOUBAN,
+                SHARE_MEDIA.SMS, SHARE_MEDIA.EMAIL, SHARE_MEDIA.YNOTE,
+                SHARE_MEDIA.EVERNOTE, SHARE_MEDIA.LAIWANG, SHARE_MEDIA.LAIWANG_DYNAMIC,
+                SHARE_MEDIA.LINKEDIN, SHARE_MEDIA.YIXIN, SHARE_MEDIA.YIXIN_CIRCLE,
+                SHARE_MEDIA.TENCENT, SHARE_MEDIA.FACEBOOK, SHARE_MEDIA.TWITTER,
+                SHARE_MEDIA.WHATSAPP, SHARE_MEDIA.GOOGLEPLUS, SHARE_MEDIA.LINE,
+                SHARE_MEDIA.INSTAGRAM, SHARE_MEDIA.KAKAO, SHARE_MEDIA.PINTEREST,
+                SHARE_MEDIA.POCKET, SHARE_MEDIA.TUMBLR, SHARE_MEDIA.FLICKR,
+                SHARE_MEDIA.FOURSQUARE, SHARE_MEDIA.MORE)
+                .addButton("umeng_sharebutton_copy", "umeng_sharebutton_copy", "umeng_socialize_copy", "umeng_socialize_copy")
+                .addButton("umeng_sharebutton_copyurl", "umeng_sharebutton_copyurl", "umeng_socialize_copyurl", "umeng_socialize_copyurl")
+                .setShareboardclickCallback(new ShareBoardlistener() {
+                    @Override
+                    public void onclick(SnsPlatform snsPlatform, SHARE_MEDIA share_media) {
+                        if (snsPlatform.mShowWord.equals("umeng_sharebutton_copy")) {
+                            Toast.makeText(ShopDetailActivity.this, "复制文本按钮", Toast.LENGTH_LONG).show();
+                        } else if (snsPlatform.mShowWord.equals("umeng_sharebutton_copyurl")) {
+                            Toast.makeText(ShopDetailActivity.this, "复制链接按钮", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            UMWeb web = new UMWeb("http://bbs.umeng.com/");
+                            web.setTitle("来自分享面板标题");
+                            web.setDescription("来自分享面板内容");
+                            web.setThumb(new UMImage(ShopDetailActivity.this, R.drawable.ic_camera));
+                            new ShareAction(ShopDetailActivity.this).withMedia(web)
+                                    .setPlatform(share_media)
+                                    .setCallback(mShareListener)
+                                    .share();
+                        }
+                    }
+                });
+    }
+
+    private static class CustomShareListener implements UMShareListener {
+
+        private WeakReference<ShopDetailActivity> mActivity;
+
+        private CustomShareListener(ShopDetailActivity activity) {
+            mActivity = new WeakReference(activity);
+        }
+
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+
+        }
+
+        @Override
+        public void onResult(SHARE_MEDIA platform) {
+
+            if (platform.name().equals("WEIXIN_FAVORITE")) {
+                Toast.makeText(mActivity.get(), platform + " 收藏成功啦", Toast.LENGTH_SHORT).show();
+            } else {
+                if (platform != SHARE_MEDIA.MORE && platform != SHARE_MEDIA.SMS
+                        && platform != SHARE_MEDIA.EMAIL
+                        && platform != SHARE_MEDIA.FLICKR
+                        && platform != SHARE_MEDIA.FOURSQUARE
+                        && platform != SHARE_MEDIA.TUMBLR
+                        && platform != SHARE_MEDIA.POCKET
+                        && platform != SHARE_MEDIA.PINTEREST
+
+                        && platform != SHARE_MEDIA.INSTAGRAM
+                        && platform != SHARE_MEDIA.GOOGLEPLUS
+                        && platform != SHARE_MEDIA.YNOTE
+                        && platform != SHARE_MEDIA.EVERNOTE) {
+                    Toast.makeText(mActivity.get(), platform + " 分享成功啦", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+
+        @Override
+        public void onError(SHARE_MEDIA platform, Throwable t) {
+            if (platform != SHARE_MEDIA.MORE && platform != SHARE_MEDIA.SMS
+                    && platform != SHARE_MEDIA.EMAIL
+                    && platform != SHARE_MEDIA.FLICKR
+                    && platform != SHARE_MEDIA.FOURSQUARE
+                    && platform != SHARE_MEDIA.TUMBLR
+                    && platform != SHARE_MEDIA.POCKET
+                    && platform != SHARE_MEDIA.PINTEREST
+
+                    && platform != SHARE_MEDIA.INSTAGRAM
+                    && platform != SHARE_MEDIA.GOOGLEPLUS
+                    && platform != SHARE_MEDIA.YNOTE
+                    && platform != SHARE_MEDIA.EVERNOTE) {
+                Toast.makeText(mActivity.get(), platform + " 分享失败啦", Toast.LENGTH_SHORT).show();
+                if (t != null) {
+                    com.umeng.socialize.utils.Log.d("throw", "throw:" + t.getMessage());
+                }
+            }
+
+        }
+
+        @Override
+        public void onCancel(SHARE_MEDIA platform) {
+
+            Toast.makeText(mActivity.get(), platform + " 分享取消了", Toast.LENGTH_SHORT).show();
         }
     }
 
