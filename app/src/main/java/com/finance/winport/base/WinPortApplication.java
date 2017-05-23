897a00f5d4fcbacdf5b8ae7e99b1eede83baf589
刘景友 <liujy@oriental-finance.com>
2017-05-23 13:46:08 +0800

    修改

--- app/src/main/java/com/finance/winport/base/WinPortApplication.java
+++ app/src/main/java/com/finance/winport/base/WinPortApplication.java
@@ -42,6 +42,7 @@ public class WinPortApplication extends Application {
         PlatformConfig.setWeixin("", "");
         PlatformConfig.setQQZone("", "");
         PlatformConfig.setSinaWeibo("4150536070", "2386b3299bcd0be389a41e9e8436e91f", "http://sns.whalecloud.com");
+//        PlatformConfig.setSinaWeibo("4134918725", "9e35ab6be6dc22d85417f50f19f44527", "http://sns.whalecloud.com");
 
     }
 
