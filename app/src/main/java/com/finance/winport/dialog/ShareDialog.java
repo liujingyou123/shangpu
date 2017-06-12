1997b2bece0d1684d05db30ed12843ee018c4145
葛进 <gejin@oriental-finance.com>
2017-06-12 13:13:41 +0800

    bug修改

--- app/src/main/java/com/finance/winport/dialog/ShareDialog.java
+++ app/src/main/java/com/finance/winport/dialog/ShareDialog.java
@@ -18,10 +18,18 @@ import com.finance.winport.log.XLog;
 import com.finance.winport.util.ToastUtil;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.ShareContent;
+import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.UMShareListener;
 import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.controller.SocialRouter;
+import com.umeng.socialize.handler.SinaSsoHandler;
+import com.umeng.socialize.handler.UMSSOHandler;
 import com.umeng.socialize.media.UMImage;
 import com.umeng.socialize.media.UMWeb;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -89,10 +97,44 @@ public class ShareDialog extends Dialog {
             umImage = new UMImage(mContext, R.drawable.default_image_logo);
         }
         web.setThumb(umImage);
-        new ShareAction((Activity) mContext).withMedia(web)
-                .setPlatform(platform)
-                .setCallback(umShareListener)
-                .share();
+
+
+        try{
+
+            UMShareAPI umShareAPI = UMShareAPI.get(getContext());
+            Field socialRouterField = umShareAPI.getClass().getDeclaredField("router");
+            socialRouterField.setAccessible(true);
+            Object socialRouterObj = socialRouterField.get(umShareAPI);
+
+            Field platformHandlersHandlerField = SocialRouter.class.getDeclaredField("platformHandlers");
+            platformHandlersHandlerField.setAccessible(true);
+            Object platformHandlersHandlerObj = platformHandlersHandlerField.get(socialRouterObj);
+            Map<SHARE_MEDIA, UMSSOHandler> platformHandlers = (Map<SHARE_MEDIA, UMSSOHandler>) platformHandlersHandlerObj;
+            platformHandlers.put(SHARE_MEDIA.SINA, new SinaSsoHandler() {
+                @Override
+                public boolean share(ShareContent content, UMShareListener listener) {
+                    try {
+                        return super.share(content, listener);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+//                        TaskExecutors.runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                Toaster.show("分享失败");
+//                            }
+//                        });
+                        return false;
+                    }
+                }
+            });
+
+            new ShareAction((Activity) mContext).withMedia(web)
+                    .setPlatform(platform)
+                    .setCallback(umShareListener)
+                    .share();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @OnClick({R.id.tv_weixin, R.id.tv_pengyou, R.id.tv_weibo, R.id.tv_qq})
