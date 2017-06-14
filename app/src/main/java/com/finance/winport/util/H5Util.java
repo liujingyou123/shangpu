f847b97d7be5912db533b15e9b588169a6a80691
刘景友 <liujy@oriental-finance.com>
2017-06-14 11:39:04 +0800

    修改H5地址方式

--- app/src/main/java/com/finance/winport/util/H5Util.java
+++ app/src/main/java/com/finance/winport/util/H5Util.java
@@ -1,19 +1,23 @@
 package com.finance.winport.util;
 
+import com.finance.winport.net.NetUtils;
+
 /**
  * Created by liuworkmac on 17/5/31.
  */
 
 public class H5Util {
-    public static String ServerIp = "http://wph5.tst.shfc999.com/";
     public static String IP_SHOP_DETAIL = "shopsInfo.html?";
     public static String IP_AGREEMENT_DETAIL = "agreement.html";
 
+    public static String getServerIp() {
+        return NetUtils.webUrl();
+    }
     public static String getIpShopDetail(String shopId) {
-        return ServerIp + IP_SHOP_DETAIL + "shopId=" + shopId;
+        return getServerIp() + IP_SHOP_DETAIL + "shopId=" + shopId;
     }
 
     public static String getIpAgreementDetail() {
-        return ServerIp + IP_AGREEMENT_DETAIL;
+        return getServerIp() + IP_AGREEMENT_DETAIL;
     }
 }
